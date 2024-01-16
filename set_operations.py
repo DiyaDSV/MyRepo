ae00103120305a3dfc44b3ea3cbf6ffd4e5b5313
DiyaDSV <108742593+DiyaDSV@users.noreply.github.com>
2024-01-16 22:43:39 +0530

    Add files via upload

---
+++ set_operations.py
@@ -0,0 +1,8 @@
+E={0,2,4,6,8}
+N={1,2,3,4,5}
+print("Set E: ",E)
+print("Set N: ",N)
+print("Union of E and N: ",E.union(N))
+print("Intersection of E and N: ",E.intersection(N))
+print("Difference of E and N: ",E.difference(N))
+print("Symmetric difference of E and N: ",E.symmetric_difference(N))
